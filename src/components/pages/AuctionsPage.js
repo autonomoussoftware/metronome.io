0a4bd48759a14c8d90be638ca625ba554c03610c
Gabriel Montes <gabriel@bloq.com>
2018-06-08 23:45:41 -0700

    Add base auctions page content

--- src/components/pages/AuctionsPage.js
+++ src/components/pages/AuctionsPage.js
@@ -1,1 +1,14 @@
-export default () => 'Auctions'
+import { connect } from 'react-redux'
+import React from 'react'
+
+import AuctionSummary from '../AuctionSummary'
+
+const AuctionsPage = ({ loadingAuctionStatus }) => (
+  loadingAuctionStatus
+    ? <div>{/* Loading... */}</div>
+    : <AuctionSummary />
+)
+
+const mapStateToProps = state => state.auction.status
+
+export default connect(mapStateToProps)(AuctionsPage)
