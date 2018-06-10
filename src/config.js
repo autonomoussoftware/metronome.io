e6c8b37fb9e8fa1ed937b0b375eed9967ba0d1f2
Gabriel Montes <gabriel@bloq.com>
2018-06-10 10:36:13 -0700

    Grab auctions contract address from lib

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+import Contracts from 'metronome-contracts'
+
 export default {
-  auctionsAddress: process.env.REACT_APP_AUCTIONS_ADDRESS,
+  auctionsAddress: Contracts.addresses[process.env.REACT_APP_NET_ID].auctions,
   defaultGasPrice: '1000000000',
   metApiUrl: process.env.REACT_APP_MET_API_URL
 }
