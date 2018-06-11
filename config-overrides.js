dc431170e6662d51d8406b6be825aca691cea4bd
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-06-11 12:10:50 -0700

    Add basic setup for webpack html plugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,17 @@
+const path = require('path')
 const rewireEslint = require('react-app-rewire-eslint')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = function override (config, env) {
-  config = rewireEslint(config, env)
-  return config
+module.exports = {
+  webpack (config, env) {
+    config = rewireEslint(config, env)
+
+    config.plugins.push(new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(__dirname, './public/features.html'),
+      filename: 'features.html'
+    }))
+
+    return config
+  }
 }
