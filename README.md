df4c3734797ce44e5a31f8493668b5ac797c8303
Gabriel Montes <gabriel@bloq.com>
2018-06-10 21:05:27 -0700

    Update chain env variable name

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 Set the following variables in a `.env` file:
 
-- `REACT_APP_NET_ID` i.e. `'mainnet'`
-- `REACT_APP_MET_API_URL` i.e. `https://api.metronome.io`
+- `REACT_APP_CHAIN`: defaults to `mainnet`
+- `REACT_APP_MET_API_URL`: defaults to `https://api.metronome.io`
 
 Then install dependencies:
 
--- src/config.js
+++ src/config.js
@@ -1,7 +1,12 @@
 import Contracts from 'metronome-contracts'
 
+const {
+  REACT_APP_CHAIN,
+  REACT_APP_MET_API_URL
+} = process.env
+
 export default {
-  auctionsAddress: Contracts.addresses[process.env.REACT_APP_NET_ID].auctions,
+  auctionsAddress: Contracts.addresses[REACT_APP_CHAIN || 'mainnet'].auctions,
   defaultGasPrice: '1000000000',
-  metApiUrl: process.env.REACT_APP_MET_API_URL
+  metApiUrl: REACT_APP_MET_API_URL || 'https://api.metronome.io'
 }
