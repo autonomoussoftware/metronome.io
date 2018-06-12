6b99b336e9a0dde0696988fdd817d5b51a6badf9
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-06-12 11:55:51 -0700

    Add download options link

--- src/components/AppsDownloadButton.js
+++ src/components/AppsDownloadButton.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 
-import { baseWalletURL, walletInstaller } from '../download-helper'
+import { currentOS, baseWalletURL, walletInstaller } from '../download-helper'
 
 class AppsDownloadButton extends Component {
   render () {
     return (
-      <a {...{ href: `${baseWalletURL}.${walletInstaller.ext}` }} className="btn">Download For {walletInstaller.os}</a>
+      <a {...{ href: `${baseWalletURL}.${walletInstaller.ext}` }} className="btn">
+        Download For {walletInstaller.os} {currentOS.version}
+      </a>
     )
   }
 }
