e5fd875f0d75df1afe9cb2699e63ec07b38f72c1
Gabriel Montes <gabriel@bloq.com>
2018-06-14 16:01:43 -0700

    Fix buy button href and target

--- src/components/AuctionBuyOptions.js
+++ src/components/AuctionBuyOptions.js
@@ -42,8 +42,7 @@ class AuctionBuyOptions extends Component {
                 <a
                   onClick={isWeb3Available && showBuyForm}
                   className="btn btn-lrg"
-                  href={isWeb3Available ? '' : useWallet.url}
-                  target="_blank">
+                  {...(isWeb3Available ? {} : { href: useWallet.url, target: '_blank' })}>
                   <span className="btn-text">
                     Buy with {isWeb3Available ? web3Provider : useWallet.name}</span>
                   <span className="btn-icon">
