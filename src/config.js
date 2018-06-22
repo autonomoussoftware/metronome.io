ae9037ec1306be14a4c473825282c8980d7f154a
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-06-22 18:48:11 -0300

    Upgrade to new wallet version

--- src/config.js
+++ src/config.js
@@ -16,7 +16,7 @@ export default {
   auctionsAddress: Contracts.addresses[defaultChain].auctions,
   chain: defaultChain,
   defaultGasPrice: '1000000000',
-  desktopAppVersion: REACT_APP_DESKTOP_APP_VERSION || 'v1.0.2',
+  desktopAppVersion: REACT_APP_DESKTOP_APP_VERSION || 'v1.0.3',
   gaTrackingId: REACT_APP_GA_TRACKING_ID || 'UA-116275666-1',
   metApiUrl: REACT_APP_MET_API_URL || 'https://api.metronome.io',
   metExplorerUrl: REACT_APP_MET_EXPLORER_URL || 'https://explorer.metronome.io',
