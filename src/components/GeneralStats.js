aa1095a2569bf943788699b8c6cf9b87e0c7d820
Pablo Enrici <pablo@bloq.com>
2018-07-10 17:13:56 -0300

    Fix dashboard header margins and responsive bahavior

--- src/components/GeneralStats.js
+++ src/components/GeneralStats.js
@@ -1,9 +1,63 @@
 import { connect } from 'react-redux'
 import React, { Component } from 'react'
 import smartRounder from 'smart-round'
-
 import CoinCapRate from '../providers/CoinCapRate'
 import MetValue from './MetValue'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  border-top: solid 1px #463f63;
+  display: flex;
+  margin: 0 auto;
+  max-width: 1140px;
+  flex-direction: column;
+
+  @media(min-width: 840px) {
+    flex-direction: row;
+  }
+`
+
+const CellContainer = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+
+  @media(min-width: 840px) {
+    justify-content: flex-start;
+  }
+`
+
+const Cell = styled.div`
+  padding: 15px;
+  border-right: solid 1px #202020;
+  border-left: ${p => p.first ? 'solid 1px #202020' : 'none'};
+`
+
+const Label = styled.div`
+  position: relative;
+  margin: 5px 0 0 0;
+  color: #fff;
+  font-size: 13px;
+`
+
+const Value = styled.div`
+  color: #45D48D;
+  font-size: 16px;
+`
+
+const Button = styled.button`
+  margin: 13px 15px;
+  display: block;
+  font-size: 14px;
+  font-weight: 600;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  text-decoration: none;
+  position: relative;
+  background-color: #fff;
+  padding: 15px 65px 15px 65px;
+  color: #7e61f8;
+`
 
 const smartRound = smartRounder(3, 0, 10)
 
@@ -23,33 +77,25 @@ class GeneralStats extends Component {
     return (
       <React.Fragment>
         <CoinCapRate onData={updateEthUsdRate}/>
-        <div className="GeneralStats container__general-stats">
-          <div className="clearInner">
-            <div className="left">
-              <div className="container__inner">
-                <div className="label__general-stats">Market Supply</div>
-                <div className="numeral__general-stats"><MetValue>{tokenSupply}</MetValue></div>
-              </div>
-            </div>
-            <div className="left">
-              <div className="container__inner">
-                <div className="label__general-stats">Percentage Sold</div>
-                <div className="numeral__general-stats">{smartRound(100 - remainingPercentage)}%</div>
-              </div>
-            </div>
-            <div className="left">
-              <div className="container__inner">
-                <div className="label__general-stats">Daily Auction Amount</div>
-                <div className="numeral__general-stats"><MetValue>{auctionSupply}</MetValue></div>
-              </div>
-            </div>
-          </div>
-          <div className="button__visit-auction-dashboard">
-            <button onClick={onBuyMetronomeClick} disabled={!isAuctionActive}>
-              Buy Metronome
-            </button>
-          </div>
-        </div>
+        <Container>
+          <CellContainer>
+            <Cell first>
+              <Label>Market Supply</Label>
+              <Value><MetValue>{tokenSupply}</MetValue></Value>
+            </Cell>
+            <Cell>
+              <Label>Percentage Sold</Label>
+              <Value>{smartRound(100 - remainingPercentage)}%</Value>
+            </Cell>
+            <Cell>
+              <Label>Daily Auction Amount</Label>
+              <Value><MetValue>{auctionSupply}</MetValue></Value>
+            </Cell>
+          </CellContainer>
+          <Button onClick={onBuyMetronomeClick} disabled={!isAuctionActive}>
+            Buy Metronome
+          </Button>
+        </Container>
       </React.Fragment>
     )
   }
