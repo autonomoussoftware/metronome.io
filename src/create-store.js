7c61aa1d70751146b5615d0dc1f7711be81a4c58
Pablo Enrici <pablo@bloq.com>
2018-07-17 13:12:24 -0300

    Disable localstorage Redux store enhancer while in development mode
    Closes #125

--- src/create-store.js
+++ src/create-store.js
@@ -1,15 +1,18 @@
 import { compose, createStore } from 'redux'
 import reduxLocalStorage from 'redux-localstorage'
+import { identity } from 'lodash'
 
 import rootReducer from './reducers'
+
+const persistState = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+  ? identity
+  : reduxLocalStorage(['auction', 'rates', 'wallet'])
 
 export default function (reduxDevtoolsOptions, initialState) {
   const composeEnhancers = (typeof window !== 'undefined' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(reduxDevtoolsOptions)
   ) || compose
-
-  const persistState = reduxLocalStorage(['auction', 'rates', 'wallet'])
 
   return createStore(
     rootReducer,
