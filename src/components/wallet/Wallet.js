a6084f3f46410dd9e63012438184a88230b19444
Pablo Enrici <pablo@bloq.com>
2019-08-13 16:16:16 -0300

    Update dynamic links in Wallet section

--- src/components/wallet/Wallet.js
+++ src/components/wallet/Wallet.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
-import DesktopDownloads from './DesktopDownloads'
-import MobileDownloads from './MobileDownloads'
 import downloadHelper from '../../download-helper'
 
 class Wallet extends Component {
@@ -12,24 +10,48 @@ class Wallet extends Component {
   }
 
   render() {
-    const {
-      walletInstallers,
-      walletInstaller,
-      baseReleaseUrl,
-      currentOS
-    } = downloadHelper(this.props.desktopAppVersion)
+    const { walletInstallers, walletInstaller, currentOS } = downloadHelper(
+      this.props.desktopAppVersion
+    )
 
     return (
       walletInstaller && (
         <React.Fragment>
-          <a className="btn btn-primary mr-4 mb-2" href={walletInstaller.url}>
-            Download For {currentOS.name} &raquo;
+          <div className="my-4" id="mobile-downloads">
+            <a
+              href={walletInstallers.iOS.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                alt="Apple download"
+                className="img-fluid mb-2 mb-sm-0"
+                src="../dist/images/apple-download.png"
+                srcSet="../dist/images/appstore-btn.svg"
+              />
+            </a>
+            &nbsp;
+            <a
+              href={walletInstallers.Android.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                alt="Android Download"
+                className="img-fluid"
+                src="../dist/images/android-download.png"
+                srcSet="../dist/images/gplay-btn.svg"
+              />
+            </a>
+          </div>
+          <a
+            className="font-weight-500"
+            href={walletInstaller.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            DOWNLOAD FOR {currentOS.name.toUpperCase()} &raquo;
           </a>
-          <DesktopDownloads
-            walletInstallers={walletInstallers}
-            baseReleaseUrl={baseReleaseUrl}
-          />
-          <MobileDownloads walletInstallers={walletInstallers} />
         </React.Fragment>
       )
     )
@@ -37,7 +59,11 @@ class Wallet extends Component {
 }
 
 const mapStateToProps = state => ({
-  desktopAppVersion: state.config.desktopAppVersion
+  desktopAppVersion: state.config.desktopAppVersion,
+  walletInstallers: PropTypes.shape({
+    Android: PropTypes.shape({ url: PropTypes.string.isRequired }).isRequired,
+    iOS: PropTypes.shape({ url: PropTypes.string.isRequired }).isRequired
+  }).isRequired
 })
 
 export default connect(mapStateToProps)(Wallet)
