16a34fa5c03dd83b907920998980fbe3d95576db
Pablo Enrici <pablo@bloq.com>
2019-08-16 15:04:46 -0300

    Improve handling of download links for unrecognized OS

--- src/components/wallet/Wallet.js
+++ src/components/wallet/Wallet.js
@@ -15,35 +15,35 @@ class Wallet extends Component {
     )
 
     return (
-      walletInstaller && (
-        <React.Fragment>
-          <div className="my-4" id="mobile-downloads">
-            <a
-              href={walletInstallers.iOS.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                alt="Apple download"
-                className="img-fluid mb-2 mb-sm-0"
-                src="../dist/images/apple-download.png"
-                srcSet="../dist/images/appstore-btn.svg"
-              />
-            </a>
-            &nbsp;
-            <a
-              href={walletInstallers.Android.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                alt="Android Download"
-                className="img-fluid"
-                src="../dist/images/android-download.png"
-                srcSet="../dist/images/gplay-btn.svg"
-              />
-            </a>
-          </div>
+      <React.Fragment>
+        <div className="my-4" id="mobile-downloads">
+          <a
+            href={walletInstallers.iOS.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt="Apple download"
+              className="img-fluid mb-2 mb-sm-0"
+              src="../dist/images/apple-download.png"
+              srcSet="../dist/images/appstore-btn.svg"
+            />
+          </a>
+          &nbsp;
+          <a
+            href={walletInstallers.Android.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt="Android Download"
+              className="img-fluid"
+              src="../dist/images/android-download.png"
+              srcSet="../dist/images/gplay-btn.svg"
+            />
+          </a>
+        </div>
+        {walletInstaller ? (
           <a
             className="font-weight-500"
             href={walletInstaller.url}
@@ -52,8 +52,38 @@ class Wallet extends Component {
           >
             DOWNLOAD FOR {currentOS.name.toUpperCase()} &raquo;
           </a>
-        </React.Fragment>
-      )
+        ) : (
+          <p>
+            Download for{' '}
+            <a
+              className="font-weight-500"
+              href={walletInstallers.Windows.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Windows
+            </a>
+            ,{' '}
+            <a
+              className="font-weight-500"
+              href={walletInstallers['Mac OS'].url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Mac OS
+            </a>{' '}
+            or{' '}
+            <a
+              className="font-weight-500"
+              href={walletInstallers.Debian.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Linux (.deb)
+            </a>
+          </p>
+        )}
+      </React.Fragment>
     )
   }
 }
